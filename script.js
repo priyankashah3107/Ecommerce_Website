3254bf08e3231c3ae68a1307dd4aa61996b20cb5
Priyanka <105877016+priyankashah3107@users.noreply.github.com>
2023-02-13 21:58:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const bar = document.querySelector('.bar');
+const nav = document.querySelector('.navbar');
+
+
+   bar.addEventListener('click', () => 
+   {
+       bar.classList.toggle('active');
+       nav.classList.toggle('active');
+   }) 
